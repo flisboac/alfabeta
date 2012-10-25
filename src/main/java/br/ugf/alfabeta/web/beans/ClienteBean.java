ebe24a9d1818d147e0bae8af2007b2f5a006a7a1
Carlos Eduardo Ferreira <ceduferreira2@gmail.com>
2012-10-25 21:26:10 -0200

    Ok Enviado.

---
+++ src/main/java/br/ugf/alfabeta/web/beans/ClienteBean.java
@@ -0,0 +1,29 @@
+
+package br.ugf.alfabeta.web.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class ClienteBean {
+    
+    private boolean usuarioLogado = false;
+    
+    public String efetuarLogin() {
+        
+        this.usuarioLogado = true;
+        return "/index.xhtml";
+    }
+    
+    public String efetuarLogoff() {
+        
+        this.usuarioLogado = false;
+        return "/cliente/index";
+    }
+    
+    public boolean isUsuarioLogado() {
+        
+        return this.usuarioLogado;
+    }
+}
