969df352c2f18fcd0d8df2b2c26deeb9c14b6fa6
Flávio Lisbôa <flisboa.costa@gmail.com>
2012-10-29 17:14:44 -0200

    CSS e aparência.

--- src/main/java/br/ugf/alfabeta/web/beans/ClienteBean.java
+++ src/main/java/br/ugf/alfabeta/web/beans/ClienteBean.java
@@ -1,25 +1,26 @@
 
 package br.ugf.alfabeta.web.beans;
 
+import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 @ManagedBean
 @SessionScoped
-public class ClienteBean {
+public class ClienteBean implements Serializable {
     
     private boolean usuarioLogado = false;
     
     public String efetuarLogin() {
         
         this.usuarioLogado = true;
-        return "/index.xhtml";
+        return "/";
     }
     
     public String efetuarLogoff() {
         
         this.usuarioLogado = false;
-        return "/cliente/index";
+        return "/cliente";
     }
     
     public boolean isUsuarioLogado() {
