10f2bdd778a60712c281e8654e384d9a5704ac2c
Carlos Eduardo Ferreira <ceduferreira2@gmail.com>
2012-11-15 22:48:50 -0200

    Alteracoes em Pedidos

---
+++ src/main/java/br/ugf/alfabeta/web/beans/VisualizacaoPedidoBean.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ugf.alfabeta.web.beans;
+
+/**
+ *
+ * @author Ana
+ */
+public class VisualizacaoPedidoBean {
+    
+}
