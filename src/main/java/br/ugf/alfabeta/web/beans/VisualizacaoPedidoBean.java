6de32fda309e560a97a7ca51818d2aa781ee5bcc
Carlos Eduardo Ferreira <ceduferreira2@gmail.com>
2012-11-16 00:02:31 -0200

    Alteracoes em Visualizacao de Pedidos e Pedidos

--- src/main/java/br/ugf/alfabeta/web/beans/VisualizacaoPedidoBean.java
+++ src/main/java/br/ugf/alfabeta/web/beans/VisualizacaoPedidoBean.java
@@ -4,10 +4,37 @@
  */
 package br.ugf.alfabeta.web.beans;
 
+import br.ugf.alfabeta.modelo.pedidos.Pedido;
+import br.ugf.alfabeta.web.util.Bean;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
 /**
  *
  * @author Ana
  */
-public class VisualizacaoPedidoBean {
-    
+    @ManagedBean
+    @ViewScoped  
+    public class VisualizacaoPedidoBean extends Bean{
+        
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+    private List<Pedido> pedidos;
+    private Pedido pedido;
 }
+
+
