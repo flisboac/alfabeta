8eaf6e5c97f0b742f59fb939ba27c3393fe9c1fb
Danillo Coradello <danillonc@live.com>
2012-11-23 00:26:22 -0200

    criacao da classe CarrinhoBean e cricao do carrinho.xhtml

---
+++ src/main/java/br/ugf/alfabeta/web/beans/CarrinhoBean.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ugf.alfabeta.web.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Admin
+ */
+@ManagedBean
+@ViewScoped
+public class CarrinhoBean {
+    
+}
